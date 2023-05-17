cb088861e72e4ebf78bd611fd269527df7a79d0c
SrDevPereira <123840329+devsrpereira@users.noreply.github.com>
2023-05-17 17:02:14 -0300

    classe Candidato e inicio da logica na classe programa

--- ContagemVotos/src/application/Program.java
+++ ContagemVotos/src/application/Program.java
@@ -1,4 +1,23 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Program {
+    public static void main(String[] args) {
+        String input = "..\\input.txt";
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(input));
+            String line = br.readLine();
+
+            while ( line != null){
+                
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+    }
 }
--- ContagemVotos/src/entities/Candidato.java
+++ ContagemVotos/src/entities/Candidato.java
@@ -1,4 +1,40 @@
 package entities;
 
 public class Candidato {
+    private String name;
+    private int vote;
+
+    public Candidato(String name, int vote) {
+        this.name = name;
+        this.vote = vote;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVote() {
+        return vote;
+    }
+
+    public void setVote(int vote) {
+        this.vote = vote;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Candidato candidato)) return false;
+
+        return getName() != null ? getName().equals(candidato.getName()) : candidato.getName() == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return getName() != null ? getName().hashCode() : 0;
+    }
 }
